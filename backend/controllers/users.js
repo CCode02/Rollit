390e10904cbcab401e7c9ce7aaa99d4ddbc5d533
CCode02 <cristiancameloariza@gmail.com>
2026-08-13 10:36:22 -0500

    Funcionalidad para obtener todos los usuarios y request creadas

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -3,4 +3,10 @@ import usersRepository from "../repository/usersRepository.js"
 
 const usersRouter = express.Router()
 
+usersRouter.get('/', (request, response) => {
+    usersRepository.getAllUsers().then((users) => {
+        response.json(users)
+    })
+})
+
 export default usersRouter
--- backend/repository/usersRepository.js
+++ backend/repository/usersRepository.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose"
 import User from "../models/user.js"
 
+const getAllUsers = () => {
+    return User.find({})
+}
+
 export default {
-    
+    getAllUsers
 }
